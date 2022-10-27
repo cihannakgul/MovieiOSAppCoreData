129b1744c69c0dc2fe3cfff45d94b3148c406b90
Cihan <cihannakgul@gmail.com>
2022-10-27 16:25:43 +0300

    Initial Commit

---
+++ MovieAppCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAppCoreData
+//
+//  Created by berkay on 27.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

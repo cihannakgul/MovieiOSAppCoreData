483833d874432a22e4c005d774b0125ff964a958
Cihan <cihannakgul@gmail.com>
2022-11-02 01:29:12 +0300

    View coredata, add item, CRUD

--- MovieAppCoreData/ViewController.swift
+++ MovieAppCoreData/ViewController.swift
@@ -1,27 +1,107 @@
 
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var nameArray = [String]()
+    var idArray = [UUID]()
     
+    
+    var selectedMovie = ""
+    var selectedMovieID : UUID?
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("git test")
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+      
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(TopRightButton))
+        
+        getData()
+    }
+    
+   @objc func getData(){
+       nameArray.removeAll(keepingCapacity: false)
+       idArray.removeAll(keepingCapacity: false)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Movies")
+        
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do{
+             let results = try context.fetch(fetchRequest)
+            
+            if results.count > 0{
+                for result in results as! [NSManagedObject]{
+                    
+                    if let name = result.value(forKey: "movieName") as? String{
+                    self.nameArray.append(name)
+                    }
+                    
+                    if let id = result.value(forKey: "id") as? UUID{
+                    self.idArray.append(id)
+                    }
+                    
+                    self.tableView.reloadData()
+                    
+                }
+            }
+          
+        }
+        
+        catch{
+            
+            print ("error : \(error)")
+        }
+        
     }
     
   
     @objc func TopRightButton(){
         
+        selectedMovie = ""
         performSegue(withIdentifier: "toDetailsVC", sender:nil)
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name("newData"), object: nil)
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        selectedMovie = nameArray[indexPath.row]
+        selectedMovieID = idArray[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "toDetailsVC"{
+            let dest = segue.destination as! toDetails
+            dest.chosenMovie = selectedMovie
+            dest.chosenMovieID = selectedMovieID
+
+            
+        }
+    }
  
 
 }
--- MovieAppCoreData/toDetails.swift
+++ MovieAppCoreData/toDetails.swift
@@ -14,8 +14,13 @@ class toDetails: UIViewController, UIImagePickerControllerDelegate, UINavigation
     
     var didImageSelected = false
     
+    
+    var chosenMovie = ""
+    var chosenMovieID : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         imageView.isUserInteractionEnabled = true
         let recognizer = UITapGestureRecognizer(target: self, action: #selector(CloseKeyboard))
@@ -23,7 +28,56 @@ class toDetails: UIViewController, UIImagePickerControllerDelegate, UINavigation
         
         imageView.addGestureRecognizer(recognizerImage)
        view.addGestureRecognizer(recognizer)
+        
+        LoadMovieInfo()
        
+    }
+    
+    func LoadMovieInfo(){
+        
+        if chosenMovie != ""{
+            
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Movies")
+            let idString = chosenMovieID?.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString!)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do{
+             
+                let results =  try context.fetch(fetchRequest)
+                if results.count>0{
+                    for result in results as! [NSManagedObject]{
+                        if let name = result.value(forKey: "movieName") as? String{
+                            txtName.text = name
+                        }
+                        
+                        if let year = result.value(forKey: "year") as? Int{
+                            txtYear.text = String(year)
+                        }
+                        
+                        if let director = result.value(forKey: "director") as? String{
+                            txtDirector.text = director
+                        }
+                        
+                        if let imageData = result.value(forKey: "image") as? Data{
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                        }
+                    }
+                }
+                    
+                
+            }
+            
+            catch{
+
+            }
+            
+            
+        }
     }
     
     @objc func SelectImage(){
@@ -69,6 +123,8 @@ class toDetails: UIViewController, UIImagePickerControllerDelegate, UINavigation
             
             do{
                 try context.save()
+                
+                WeMadeIt()
             }
             catch{
                
@@ -82,6 +138,19 @@ class toDetails: UIViewController, UIImagePickerControllerDelegate, UINavigation
             AlertMessageBuilder(Message: "Please select an image")
         }
      
+        
+        func WeMadeIt(){
+            
+            AlertMessageBuilder(Message: "\(txtName.text!) movie added to list!")
+            txtName.text=""
+            txtDirector.text=""
+            txtYear.text=""
+            imageView.image = UIImage(named: "selectImage")
+            
+            NotificationCenter.default.post(name: NSNotification.Name("newData"), object: nil)
+                 
+        
+        }
         
         func AlertMessageBuilder(Message:String){
             let alert = UIAlertController(title: "Warning", message: Message, preferredStyle: .alert)
